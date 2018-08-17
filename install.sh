b4249f495d9bbc04c37ce2e87fd0c8e7a2e1b583
Rodolfo Navalon <navalonrodolfo@gmail.com>
2018-08-17 00:41:03 -0400

    Fixed android sdk and pakages installation

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 
 before_install()
 {
-  echo "Executing Before Install..."
+  echo -e "\n\nExecuting Before Install...\n\n"
   # Installing the pyton command to add repository in debian
   apt-get install -y software-properties-common
   apt-get install dirmngr
+  apt-get install -y curl
 }
 
 after_install()
@@ -20,7 +21,7 @@ set_up_environment_variables()
 
 install_java()
 {
-  echo "Installing Java..."
+  echo -e "\n\nInstalling Java...\n\n"
   # Adds the java repository, repo, keys and update the apt. This also append the repo urls to
   # the sources file and making sure that we do not append same deb and deb source
   # all over again when running the installation.
@@ -37,7 +38,7 @@ install_java()
 
 install_android_sdk()
 {
-  echo "Installing Android SDK..."
+  echo -e "\n\nInstalling Android SDK...\n\n"
   # Install the android sdk tools manually through the zip file, this is to be
   # able to do fastbooting to the android cli and prevent unnecessary tools that
   # would bloat the memory with junk tools. Only install what we needed.
@@ -55,13 +56,17 @@ install_android_sdk()
   if [ ! -d "$ANDROID_SDK_DIRECTORY_TOOLS" ] || 
      [ ! -d "$ANDROID_SDK_DIRECTORY_TOOLS_BINARY" ] || 
      [ ! -d "$ANDROID_SDK_DIRECTORY_PLATFORM_TOOLS" ]; then
+    rm -rf ANDROID_HOME
     curl -L $ANDROID_SDK_URL -o $ANDROID_SDK_FILE
     mkdir -p $ANDROID_HOME
     pushd $ANDROID_HOME
     unzip -x $ANDROID_SDK_FILE
     popd
     rm $ANDROID_SDK_FILE
+  else
+    echo "Android SDK has already been installed"
   fi
+
   # Create a shell script that would export all of the path and home directory
   # of the android sdk to the profile which is sharable to all of the users.
   EXPORT_SDK_HOME="export ANDROID_HOME=$ANDROID_HOME"
@@ -70,24 +75,28 @@ install_android_sdk()
   # Set the sdk tools to be executable by all of the user in the system.
   chmod +x -R $ANDROID_SDK_DIRECTORY_TOOLS
   chmod +x -R $ANDROID_SDK_DIRECTORY_TOOLS_BINARY
-  chmod +x -R $ANDROID_SDK_DIRECTORY_PLATFORM_TOOLS
+  # Set the path and home within sudo
+  eval "$EXPORT_SDK_HOME"
+  eval "$EXPORT_SDK_PATH"
+  echo "Setting the PATH and HOME environment variables within the user profile... Done"
 }
 
 install_android_sdk_manager_packages()
 {
-  echo "Installing Android SDK Manager Packages..."
+  echo -e "\n\nInstalling Android SDK Manager Packages...\n\n"
   yes | sdkmanager --licenses
   sdkmanager --verbose \
 	     "platform-tools" \
 	     "tools" \
-	     "platforms;android-"{21,27} \
-             "build-tools;"{27.0.3}
+	     "platforms;android-"{21,22,23,24,25,26,27} \
+             "build-tools;"{27.0.3,28.0.2}
   sdkmanager --verbose --update
+  chmod +x -R $ANDROID_SDK_DIRECTORY_PLATFORM_TOOLS
 }
 
 registerRunner()
 {
-  echo "Registering Gitlab Runner..."
+  echo -e "\n\nRegistering Gitlab Runner...\n\n"
 }
 
 unregisterRunner()
@@ -99,8 +108,8 @@ main ()
 {
   before_install
   install_java
-#  install_android_sdk
-#  install_android_sdk_manager_packages
+  install_android_sdk
+  install_android_sdk_manager_packages
 }
 
 main
